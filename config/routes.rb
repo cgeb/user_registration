b68c263da2648aba9815db4166af6d28b32ec161
cgeb <ckg61386@gmail.com>
2019-07-07 18:58:56 +0900

    use login and logout as URL for sessions new and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new', as: "login"
+  get 'login', to: "sessions#new", as: "login"
   post 'sessions/create'
-  delete 'sessions/destroy', as: "logout"
+  delete 'logout', to: 'sessions#destroy', as: "logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
