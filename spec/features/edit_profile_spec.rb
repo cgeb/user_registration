e0387a9664c94ea2f580a5c54758cfa76b462112
cgeb <ckg61386@gmail.com>
2019-07-07 18:32:23 +0900

    remove unneeded code created because using generators and fix failing test

--- spec/features/edit_profile_spec.rb
+++ spec/features/edit_profile_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe "edit profile" do
     it "allows user to edit profile" do
       visit(edit_user_path(user))
       fill_in("Username", with: "ckg61386")
+      fill_in("Password", with: "password2")
+      fill_in("Password confirmation", with: "password2")
       click_button("Update")
       expect(current_path).to eq(user_path(user))
       expect(page).to have_content("ckg61386")
